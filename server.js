e1caeb97c125ae6e13abacb34049395f4ab455fb
adminuser <adminuser@localhost.localdomain>
2017-08-01 17:37:09 -0400

    Setup start script and Heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
@@ -14,17 +16,17 @@ app.set('view engine', 'hbs');
 
 app.use(express.static(__dirname + '/public'));
 
-app.use((req, res, next) => {
-  let now = new Date().toString();
-  log = `${now}: ${req.method} ${req.url}`;
-  console.log(log);
-  fs.appendFile('server.log', log + '\n', (err) => {
-    if (err) {
-      console.log('Unable to append to server log.');
-    }
-  });
-  next();
-});
+// app.use((req, res, next) => {
+//   let now = new Date().toString();
+//   log = `${now}: ${req.method} ${req.url}`;
+//   console.log(log);
+//   fs.appendFile('server.log', log + '\n', (err) => {
+//     if (err) {
+//       console.log('Unable to append to server log.');
+//     }
+//   });
+//   next();
+// });
 
 hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
@@ -43,6 +45,6 @@ app.get('/about', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
